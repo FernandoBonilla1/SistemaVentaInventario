17f48b3d651d84bd9569be729f1b8d0d0d69054f
Fernando Bonilla <fbr009@alumnos.ucn.cl>
2022-06-27 23:31:30 -0400

    Sale

--- src/controllers/sales.controllers.js
+++ src/controllers/sales.controllers.js
@@ -1,10 +1,21 @@
 const connection = require('../config/db');
 
+const generateRandomString = (num) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result1 = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < num; i++) {
+        result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result1;
+}
+
 const getSale = async (req, res) => {
     try {
-        const {id} = req.body
-        const sales = await connection.query('Select sale.id as id_sale, details.id_product as id_product, product.name as nombre, details.amount as amount, product.value as price_unit, details.price as price from sale inner join details on (sale.id = details.id_sale) inner join product on (details.id_product = product.id) WHERE sale.id = $1',[id]);
-        if(sales.rows.length === 0){
+        const { id } = req.body
+        const sales = await connection.query('Select sale.id as id_sale, details.id_product as id_product, product.name as nombre, details.amount as amount, product.value as price_unit, details.price as price from sale inner join details on (sale.id = details.id_sale) inner join product on (details.id_product = product.id) WHERE sale.id = $1', [id]);
+        if (sales.rows.length === 0) {
             return res.status(200).json({
                 msg: "No hay productos en la venta"
             })
@@ -24,11 +35,12 @@ const addSale = async (req, res) => {
         const date = Date.now();
         const hoy = new Date(date);
         const fecha_actual = hoy.toLocaleDateString();
-        console.log(fecha_actual)
-        const sale = await connection.query('INSERT INTO sale(id_cliente,id_salesman,date) VALUES($1,$2,$3)', [id_cliente, id_salesman, fecha_actual]);
-        const ultimoid = await connection.query('select sale.id from sale order by sale.id desc limit 1');
+        const cant = await connection.query('SELECT count(*) from sale')
+        const id = `${generateRandomString(7) + cant.rows[0].count}`
+        const id_sale = id.trim()
+        const sale = await connection.query('INSERT INTO sale(id,id_cliente,id_salesman,date) VALUES($1,$2,$3,$4)', [id_sale, id_cliente, id_salesman, fecha_actual]);
         res.status(200).json({
-            id: ultimoid.rows[0].id
+            id: id_sale
         });
     } catch (error) {
         res.status(500).json({
@@ -37,20 +49,27 @@ const addSale = async (req, res) => {
     }
 }
 
-const removeProductToSale = async(req, res) =>{
-    try{
-        const {id, id_product} = req.body;
+const removeProductToSale = async (req, res) => {
+    try {
+        const { id, id_product } = req.body;
         if (id == "") {
             return res.status(400).json({
                 msg: "Debe especificar el producto"
             })
         } else {
-            const product = await connection.query('DELETE FROM details WHERE id_sale = $1 and id_product = $2', [id,id_product]);
-            res.status(200).json({
-                msg: `Se elimino el producto de la venta.`
-            })
+            const product1 = await connection.query('SELECT * FROM details Where id_sale = $1 and id_product = $2', [id, id_product]);
+            if (product1.rows.length === 0) {
+                return res.status(200).json({
+                    msg: "El producto no esta en la venta"
+                })
+            } else {
+                const product2 = await connection.query('DELETE FROM details Where id_sale = $1 and id_product = $2', [id, id_product]);
+                res.status(200).json({
+                    msg: `Se elimino el producto de la venta.`
+                })
+            }
         }
-    } catch (error){
+    } catch (error) {
         res.status(500).json({
             msg: "No se pudo acceder a la tabla detalles"
         })
@@ -92,26 +111,26 @@ const addProductToSale = async (req, res) => {
 }
 
 const confirmsale = async (req, res) => {
-    try{
-        const {id,id_payment_method} = req.body
-        const sale = await connection.query("UPDATE sale SET id_payment_method = $1 WHERE id = $2",[id_payment_method,id])
-        try{
-            const details = await connection.query("Select sale.id as id_sale, details.id_product as id_product, details.amount as purchased_amount, product.amount as product_amount, details.price as price from sale inner join details on (sale.id = details.id_sale) inner join product on (details.id_product = product.id) Where sale.id = $1",[id]);
+    try {
+        const { id, id_payment_method } = req.body
+        const sale = await connection.query("UPDATE sale SET id_payment_method = $1 WHERE id = $2", [id_payment_method, id])
+        try {
+            const details = await connection.query("Select sale.id as id_sale, details.id_product as id_product, details.amount as purchased_amount, product.amount as product_amount, details.price as price from sale inner join details on (sale.id = details.id_sale) inner join product on (details.id_product = product.id) Where sale.id = $1", [id]);
             var price = 0
-            for(var i = 0; i < details.rows.length;i++){
+            for (var i = 0; i < details.rows.length; i++) {
                 var amount = details.rows[i].product_amount - details.rows[i].purchased_amount;
                 price = price + details.rows[i].price
-                var update = await connection.query("UPDATE product SET amount = $1 WHERE id = $2",[amount,details.rows[i].id_product])
+                var update = await connection.query("UPDATE product SET amount = $1 WHERE id = $2", [amount, details.rows[i].id_product])
             }
             res.status(200).json({
                 msg: `Se completo la venta el total a pagar de la venta con id: ${id} es: $${price}`
             });
-        }catch(error){
+        } catch (error) {
             res.status(500).json({
                 msg: "No se pudo acceder a la tabla detalles"
             })
         }
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
             msg: "No se pudo acceder a la tabla ventas"
         })
--- src/test.js
+++ src/test.js
@@ -1,5 +1,13 @@
-const date = Date.now();
-const hoy = new Date(date);
-const fecha_actual = hoy.toLocaleDateString()
+const connection = require('../config/db');
+const generateRandomString = (num) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result1 = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < num; i++) {
+        result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
 
-console.log(fecha_actual)
+    return result1;
+}
+
+
